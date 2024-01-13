5b2e6a610ff868c561726bec9f1d264c71550fe0
joseMarciano <marcianojosepaulo@gmail.com>
2024-01-12 21:11:25 -0300

    feat: :sparkles: using functions using many values

--- fundacao/func/func.go
+++ fundacao/func/func.go
@@ -10,6 +10,7 @@ func main() {
 	fmt.Println(sum(80, 79))
 	fmt.Println(sumOrError(10, 10))
 	fmt.Println(sumOrError(80, 79))
+	fmt.Println(sumAll(1, 2, 3, 4, 5, 6, 7, 8, 1))
 }
 
 func sum(a, b int) (int, bool) {
@@ -20,6 +21,15 @@ func sum(a, b int) (int, bool) {
 	}
 
 	return result, false
+}
+
+func sumAll(values ...int) int {
+	var total = 0
+	for _, v := range values {
+		total += v
+	}
+
+	return total
 }
 
 func sumOrError(a, b int) (int, error) {
