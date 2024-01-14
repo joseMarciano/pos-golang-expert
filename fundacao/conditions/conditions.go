fa1351f36cf55f2466aaa3db29e9d2e9f575e359
joseMarciano <marcianojosepaulo@gmail.com>
2024-01-14 19:51:49 -0300

    feat: :sparkles: using switch

---
+++ fundacao/conditions/conditions.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+	var name = "John"
+	switch name {
+	case "John":
+		println("Hello John")
+	case "Doe":
+		println("Hello Doe")
+	default:
+		println("Hello")
+	}
+}
