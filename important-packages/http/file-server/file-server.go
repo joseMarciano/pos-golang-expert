dc6401d449f5c85d549d154086727df7e9af320d
joseMarciano <marcianojosepaulo@gmail.com>
2024-01-22 21:11:20 -0300

    feat: :sparkles: creating  file server

---
+++ important-packages/http/file-server/file-server.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"path/filepath"
+)
+
+func main() {
+	// go run file-server.go
+	pwd, _ := filepath.Abs("./public")
+	log.Println(pwd)
+	fileServer := http.FileServer(http.Dir(pwd))
+	mux := http.NewServeMux()
+
+	mux.Handle("/", fileServer)
+	log.Fatal(http.ListenAndServe(":8080", mux))
+}
