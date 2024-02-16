8b42a1c0550bc1f05787168d244a957629ed1f28
joseMarciano <marcianojosepaulo@gmail.com>
2024-02-16 09:30:48 -0300

    feat: :sparkles: creating a product using a server

--- apis/cmd/server/main.go
+++ apis/cmd/server/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
-	"fmt"
-	"github.com/joseMarciano/pos-golang-expert/apis/configs/authconfig"
-	"github.com/joseMarciano/pos-golang-expert/apis/configs/databaseconfig"
-	"github.com/joseMarciano/pos-golang-expert/apis/configs/serverconfig"
+	"encoding/json"
+	"github.com/joseMarciano/pos-golang-expert/apis/internal/dto"
+	"github.com/joseMarciano/pos-golang-expert/apis/internal/entity/product"
+	"github.com/joseMarciano/pos-golang-expert/apis/internal/entity/user"
+	"github.com/joseMarciano/pos-golang-expert/apis/internal/infra/database"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+	"net/http"
 )
 
 func init() {
@@ -12,7 +16,55 @@ func init() {
 }
 
 func main() {
-	fmt.Println(databaseconfig.GetDbConfig())
-	fmt.Println(authconfig.GetAuthConfig())
-	fmt.Println(serverconfig.GetServerConfig())
+	//fmt.Println(databaseconfig.GetDbConfig())
+	//fmt.Println(authconfig.GetAuthConfig())
+	//fmt.Println(serverconfig.GetServerConfig())
+
+	db, err := gorm.Open(sqlite.Open("test.db"), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&user.User{}, &product.Product{})
+
+	productDB := database.NewProductDB(db)
+	productHandler := NewProductHandler(productDB)
+
+	http.HandleFunc("/products", productHandler.CreateProduct)
+	http.ListenAndServe(":8080", nil)
+
+}
+
+type ProductHandler struct {
+	ProductDB database.ProductInterface
+}
+
+func NewProductHandler(db database.ProductInterface) *ProductHandler {
+	return &ProductHandler{ProductDB: db}
+}
+
+func (p *ProductHandler) CreateProduct(w http.ResponseWriter, r *http.Request) {
+	var productInput dto.CreateProductInput
+	err := json.NewDecoder(r.Body).Decode(&productInput)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Error on decode body"))
+		return
+	}
+
+	pr := product.NewProduct(productInput.Name, productInput.Price)
+
+	err = p.ProductDB.Create(pr)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	err = json.NewEncoder(w).Encode(pr)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		return
+	}
+
 }
